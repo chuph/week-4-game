564547725cbf115069338dbbb76ead4c50b77ed7
Phillip Chu <phillip.chu1@gmail.com>
2017-05-18 23:38:16 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,14 +8,26 @@ var userNumber = 0;
 // Get target number
 function targetNum() {
 
-var targetNumber = Math.floor(Math.random() * 100);
+var targetNumber = Math.floor(Math.random() * 101);
 
 $("#goalNumber").text(targetNumber);
 
-}
+};
+
+// Add value to crystals
+
+function crystalpoints () {
+
+	for (var = c; c < 4; c++) {
+		var userPoints = Math.floor(Math.random() * 16);
+
+		$("#power" + c).attr("value", userPoints);
+	};
+};
 
 
 
 targetNum();
+crystalpoints();
 
 
